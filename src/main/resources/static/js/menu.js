8967bf703745686b4c9aa056307eb6805273ff95
perezlaborda@gmail.com <perezlaborda@gmail.com>
2014-10-24 23:27:46 -0300

    updated menu page

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -7,13 +7,46 @@ angular.module('menuApp', [])
             $http.get("/memory")
                 .success(function(response) {
                     $scope.memo = true;
+                    $scope.fs = false;
+                    $scope.ps = false;
                     $scope.totalMem = response.totalMemory;
                     $scope.usedMem = response.usedMemory;
                     $scope.freeMem = response.freeMemory;
                 })
-                .error(function(response){
+                .error(function(status){
                     $scope.memo = undefined;
                     console.log("error: "+response);
+                });
+        };
+        $scope.getFileSystemUsage = function() {
+            $http.get("/filesystem")
+                .success(function (response) {
+                    console.log(response);
+                    $scope.fileSystems = response;
+                    $scope.memo = false;
+                    $scope.ps = false;
+                    $scope.fs = true;
+                })
+                .error(function(response) {
+                    $scope.memo = false;
+                    $scope.fs = false;
+                    console.log("error: " + response);
+                });
+        };
+        $scope.getProcess = function () {
+            $http.get("/proceso")
+                .success(function (response) {
+                    console.log(response);
+                    $scope.process = response;
+                    $scope.fs = false;
+                    $scope.memo = false;
+                    $scope.ps = true;
+                })
+                .error(function(response){
+                    $scope.memo = false;
+                    $scope.fs = false;
+                    $scope.ps = false;
+                    console.log("error: " + response);
                 });
         };
     }]);
